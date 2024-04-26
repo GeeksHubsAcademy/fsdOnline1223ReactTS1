520d6adf965dc765c00c28e4057d5693b94efbf8
Dave86dev <davidochando1986@gmail.com>
2024-04-26 21:26:35 +0200

    feat: comments added

--- src/common/CButton/CButton.tsx
+++ src/common/CButton/CButton.tsx
@@ -1,6 +1,9 @@
 import "./CButton.css";
 import { CustomButton } from "../../interfaces";
 
+//NO olvidemos que las funciones que reciben props tienen que setear éstas mismas, es decir,
+//primero seteamos el componente funcional como que es de tipo React.FC y posteriormente las props como que forman parte de una interfaz
+//global llamada CustomButton
 export const CButton: React.FC<CustomButton> = ({ title, design, onClick }) => {
   return (
     <div className={design} onClick={() => onClick()}>
--- src/services/apiCalls.ts
+++ src/services/apiCalls.ts
@@ -37,7 +37,9 @@ export const logMeIn = async (credentials: Credentials): Promise<DataFetched> =>
   
   try {
     const response: any = await fetch(`http://localhost:4000/api/auth/login`, options);
-    
+    //aqui en data recibimos la respuesta pura de la api, le hemos asignado la interfaz DataFetched la cual espera 3 cosas
+    //un booleano success para saber si ha ido todo bien, un string message con el mensaje y data que es un array con any donde podremos incluir los datos de vuelta en un formato de array 
+    //capaz de admitir cualquier tipo de datos. 
     const data: DataFetched = await response.json();
 
     if(!data.success){
@@ -46,6 +48,8 @@ export const logMeIn = async (credentials: Credentials): Promise<DataFetched> =>
 
     return data;
   } catch (error: any) {
+    //en este caso estamos formateando la respuesta de error que hemos provocado al recibir un false en success.
+    //devolveremos mensaje y success como false y seteamos data a un array vacio
     let answer: DataFetched = {
       message: error.message,
       data: [],
